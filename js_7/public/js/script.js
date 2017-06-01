490a5077cdd7287d65e1458590eb06c69032ed41
Artem Romanov <artdoua@gmail.com>
2017-06-01 18:57:55 +0300

    HW_JS_7 Demo

--- js_7/public/js/script.js
+++ js_7/public/js/script.js
@@ -1,63 +1,67 @@
 (function(){
     $(document).ready(function(){
-//        $('.tabBox__item').hide();
-//        $('.tabBox__item-select').show();
-//        $('.tabBox__link').click(function(){
-//            console.log(arguments);
-//
-//            return false;
-//        });
-//        //console.log($('.tabBox__link'));
-//
-//
-//       // $('.tabBox__head').;
-
         $('ul#tabBox__head').each(function(){
-            // For each set of tabs, we want to keep track of
-            // which tab is active and its associated content
             var $active, $content, $links = $(this).find('a');
-            console.log($active);
-            // If the location.hash matches one of the links, use that as the active tab.
-            // If no match is found, use the first link as the initial active tab.
             $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
             $active.addClass('active');
-
             $content = $($active[0].hash);
-
-            // Hide the remaining content
             $links.not($active).each(function () {
                 $(this.hash).hide();
             });
-            $($active).focus();
-            // Bind the click event handler
-//            function hendler(event){
-//                // console.log(event.code);
-////                if(event.code=='Tab'){
-//                    alert(event);
-////                }
-//            }
-//            window.addEventListener('keypress', hendler);
-//            $('a').on("blur", function(){
-//               // $(this).focus();
-//                //e.preventDefault();
-//            });
+            $($(".li>a")[0]).focus();
             $('a').on("focus", function(e){
-                // Make the old tab inactive.
+                $active.removeClass('active');
+                $active = $(this);
+                $active.click();
+            });
+            $('a').on("click", function(e){
                 $active.removeClass('active');
                 $content.hide();
-                // Update the variables with the new link and content
                 $active = $(this);
                 $content = $(this.hash);
-
-                // Make the tab active.
                 $active.addClass('active');
                 $content.show();
-
-                // Prevent the anchor's default click action
-                //e.preventDefault();
+                //console.log($active);
                 return false;
             });
         });
+        var $tipEl = $('<p>');
+        $tipEl.css({
+            border: '1px solid #515151',
+            backgroundColor: '#fefefe',
+            padding: '3px',
+            position: 'absolute',
+            left: '65%',
+            borderRadius: '5px'
+        });
+        var tip, timer;
+        $(".toolTip__target").on('focus mouseenter',function(t){
+            if(!timer){
+                tip = $(this).attr('title');
+                $tipEl.text(tip);
+               // $(this).attr('title', '');
+                $($tipEl).hide();
+                $(this).after($tipEl);
+                $($tipEl).fadeIn(200);
+                timer = setTimeout(function(){
+                    $($tipEl).fadeOut(200);
+                    return false;
+                }, 2000);
+            }
+            return false;
+        })
+            .on('blur mouseleave', function(){
+                $($tipEl).hide();
+                clearTimeout(timer);
+                timer=0;
+               // $(this).attr('title', tip);
+                return false;
 
+            });
+        $('input[type=button]').on('click',function(){
+            $(".toolTip__target").each(function(){
+               $(this).focus();
+            });
+        });
     });
 })();
