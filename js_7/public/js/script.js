1e79df80114f9bbdb1afa53f639d52f3e37e6502
Artem Romanov <artdoua@gmail.com>
2017-06-03 20:17:48 +0300

    HWJS7 Complitt

--- js_7/public/js/script.js
+++ js_7/public/js/script.js
@@ -25,39 +25,46 @@
                 return false;
             });
         });
-        var $tipEl = $('<p>');
-        $tipEl.css({
-            border: '1px solid #515151',
-            backgroundColor: '#fefefe',
-            padding: '3px',
-            position: 'absolute',
-            left: '65%',
-            borderRadius: '5px'
-        });
-        var tip, timer;
-        $(".toolTip__target").on('focus mouseenter',function(t){
-            if(!timer){
-                tip = $(this).attr('title');
-                $tipEl.text(tip);
-               // $(this).attr('title', '');
-                $($tipEl).hide();
-                $(this).after($tipEl);
-                $($tipEl).fadeIn(200);
-                timer = setTimeout(function(){
-                    $($tipEl).fadeOut(200);
-                    return false;
-                }, 2000);
-            }
-            return false;
-        })
-            .on('blur mouseleave', function(){
-                $($tipEl).hide();
-                clearTimeout(timer);
-                timer=0;
-               // $(this).attr('title', tip);
-                return false;
-
+        //--------------------------------------------------****--------------------------------
+        function TipsConst($Element) {
+            var $tipEl = $('<p>');
+            $tipEl.css({
+                border: '1px solid #515151',
+                backgroundColor: '#fefefe',
+                padding: '3px',
+                position: 'absolute',
+                left: '65%',
+                borderRadius: '5px'
             });
+            var tip, timer;
+            $Element.on('focus mouseenter', function (t) {
+                if (!timer) {
+                    tip = $(this).attr('title');
+                    $tipEl.text(tip);
+                    // $(this).attr('title', '');
+                    $($tipEl).hide();
+                    $(this).after($tipEl);
+                    $($tipEl).fadeIn(200);
+                    timer = setTimeout(function () {
+                        $($tipEl).fadeOut(200);
+                        return false;
+                    }, 2000);
+                }
+                return false;
+            })
+                .on('blur mouseleave', function () {
+                    //$($tipEl).hide();
+                    //clearTimeout(timer);
+                    timer = 0;
+                    // $(this).attr('title', tip);
+                    return false;
+
+                });
+        }
+        var tipName = new TipsConst($('#tipName'));
+        var tipLastname = new TipsConst($('#tipLastname'));
+        var tipAddres = new TipsConst($('#tipAddres'));
+
         $('input[type=button]').on('click',function(){
             $(".toolTip__target").each(function(){
                $(this).focus();
