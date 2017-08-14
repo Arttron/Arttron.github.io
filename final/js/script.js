633cd41dd3018b52361c4ff618f125b29dba1fe7
Artem Romanov <artdoua@gmail.com>
2017-08-14 14:02:29 +0300

    final search add

--- final/js/script.js
+++ final/js/script.js
@@ -25,6 +25,59 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
         });
     });
 })();
+/**
+ * Created by User on 14.08.2017.
+ */
+(function () {
+    function pixaBayRequest() {
+        var query = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "city";
+        var page = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+
+        var serchRes = {};
+        var key = "5780982-5f1903b9b55e403f26dbb04a1";
+        var pixaBayHTTP = new XMLHttpRequest();
+        pixaBayHTTP.open("GET", "https://pixabay.com/api/?key=" + key + "&q=" + query + "&page=" + page + "&per_page=7&image_type=photo", true);
+        pixaBayHTTP.onreadystatechange = function () {
+            if (pixaBayHTTP.readyState == 4) {
+                if (pixaBayHTTP.status == 200) {
+                    console.log(JSON.parse(pixaBayHTTP.responseText));
+                    renderIdeasTenplate(JSON.parse(pixaBayHTTP.responseText));
+                    var grid = document.querySelector('.grid');
+                    var msnry = new Masonry(grid, {
+                        // options...
+                        columnWidth: '.grid-sizer',
+                        // do not use .grid-sizer in layout
+                        itemSelector: '.grid-item',
+                        percentPosition: true,
+                        gutter: 10
+                    });
+                }
+            }
+        };
+        pixaBayHTTP.send("null");
+        return serchRes;
+    }
+    function renderIdeasTenplate(data) {
+        var reg = /^[\w \ ]+/g;
+        var i = 0;
+        _.forEach(data.hits, function (item) {
+            console.log(item.tags.match(reg)[0]);
+            data.hits[i].tags = item.tags.match(reg)[0];
+            i++;
+        });
+        var partnersTmpl = _.template(document.querySelector("#IdeasTemplate").innerText);
+        document.querySelector("#ideas__api-img").innerHTML = partnersTmpl(data);
+    }
+    pixaBayRequest();
+    var searchBtn = document.querySelector("#search__button");
+    var searchText = document.querySelector('#search__input');
+
+    searchBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+        console.log(searchText.value);
+        pixaBayRequest(searchText.value);
+    });
+})();
 'use strict';
 (function (document) {
     var Parent = function Parent() {
@@ -62,19 +115,10 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     }
     function renderTenplate(data) {
         var partnersTmpl = _.template(document.querySelector("#PartnersTemplate").innerText);
-
+        console.log(data);
         document.querySelector("#partners-cont").innerHTML = partnersTmpl(data);
     }
     myJsonRequest();
     // vanilla JS
     // init with element
-    var grid = document.querySelector('.grid');
-    var msnry = new Masonry(grid, {
-        // options...
-        columnWidth: '.grid-sizer',
-        // do not use .grid-sizer in layout
-        itemSelector: '.grid-item',
-        percentPosition: true,
-        gutter: 10
-    });
 })(document);
